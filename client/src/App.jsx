343c1d614dd63fd8cae7025871612be346dc7f24
Stuart Kaija <stuartkaija@gmail.com>
2022-01-23 18:59:56 -0800

    basic tinder card component created, photos added to server for mock data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header/Header';
 import LoginPage from './pages/LoginPage/LoginPage';
 import SignUp from './pages/SignUpPage/SignUpPage';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
+import FindBuddyPage from './pages/FindBuddyPage/FindBuddyPage';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
             <Switch>
                 <Route path='/' exact component={LoginPage} />
                 <Route path='/signup' component={SignUp} />
+                <Route path='/find' component={FindBuddyPage} />
                 <Route path='/:userId' component={ProfilePage} />
             </Switch>
         </BrowserRouter>
