d66afab39812733766afea633467ec31faf51dd8
Stuart Kaija <stuartkaija@gmail.com>
2022-01-25 14:54:06 -0800

    basic weather data returning and populating front end

--- client/src/pages/SearchPage/SearchPage.jsx
+++ client/src/pages/SearchPage/SearchPage.jsx
@@ -4,38 +4,56 @@ import axios from 'axios';
 export default class SearchPage extends Component {
 
     // state to hold the data when it arrives from the backend
-
+    state = {
+        location: null,
+    }
 
     handleSearch = (event) => {
         event.preventDefault();
-        const userSearch = event.target.search.value
+        const userInput = event.target.search.value
         axios
             .post("http://localhost:8080/weather", {
-                search: userSearch
+                search: userInput
             })
             .then(response => {
-                console.log(response);
+                console.log(response.data);
+                this.setState({
+                    location: response.data
+                });
             })
             .catch(error => console.log(error));
     };
 
     render() {
+        if (this.state.location === null) {
+            return (
+                <>
+                    <form onSubmit={this.handleSearch} id='searchForm'>
+                    <label htmlFor="">Search for a location</label>
+                    <input type="search" name='search' id='search' />
+                    </form>
+                    <button form='searchForm'>Search</button>
+                </>
+            )
+        }
+
+        // const { conditions, temperature, visibility, wind } = this.state.location.current
+
         return (
             <>
                 <form onSubmit={this.handleSearch} id='searchForm'>
-                    <label htmlFor="">Search for a dive spot</label>
+                    <label htmlFor="">Search for a location</label>
                     <input type="search" name='search' id='search' />
                 </form>
                 <button form='searchForm'>Search</button>
                 <div>
                     {/* returned data just to get it on the page in the next hour hopefully */}
-                    <p>condition</p>
-                    <p>temp</p>
-                    <p>wind</p>
-                    <p>vis</p>
-                    <p>swell</p>
-                    <p>tide</p>
+                    <p>Conditions: {this.state.location.current.condition.text}</p>
+                    <p>Temperature: {this.state.location.current.temp_c}</p>
+                    <p>Visibility: {this.state.location.current.vis_km}km</p>
+                    <p>Wind: {this.state.location.current.wind_kph}km {this.state.location.current.wind_dir}</p>
                 </div>
+                <button>Marine Info</button>
             </>
         );
     }
--- server/routes/weatherRoutes.js
+++ server/routes/weatherRoutes.js
@@ -16,7 +16,9 @@ router.post('/', (req, res) => {
     axios
         .get(WEATHER_API_URL + REACT_APP_WEATHER_API_KEY + "&q=" + userInput)
         // .get("http://api.weatherapi.com/v1/current.json?key=6de91db3616a4f0aa9a04248222501&q=Utila")
-        .then(response => console.log(response.data))
+        // .then(response => console.log(response.data.location))
+        .then(response => res.status(200).json(response.data))
+        .catch(error => console.log(error));
         // .then(axios
         //         .get("http://api.worldweatheronline.com/premium/v1/marine.ashx?key=6270c0b02c5c4f02a0641150222501&q=15.65,-86.35")
         //         .then()
